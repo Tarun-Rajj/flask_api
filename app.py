5715884b128c40ef71b525857db198be2f6897b5
Tarun-Rajj <tarunkumar86437@gmail.com>
2023-11-09 10:42:37 +0530

    remove comment part and hide data.json file

--- app.py
+++ app.py
@@ -3,23 +3,6 @@ import json
 
 app = Flask(__name__)
 
-# input = [
-#     {
-#         'id': 1,
-#         'title': 'My_First_API',
-#         'description': 'Take_Input, Title, description,',
-#         'done': False
-#     },
-#     {
-#         'id': 2,
-#         'title': 'Learn Python',
-#         'description': 'Need to find a good Python tutorial on the web',
-#         'done': False
-#     }
-# ]
-
-
-
 @app.route('/store_input',methods=['POST'])
 def store_input():
             try:
